fbb5e353a1c7056ca8455f673d6459669e543a21
17-shalvi <71483138+17-shalvi@users.noreply.github.com>
2023-03-26 07:01:50 -0400

    Added Text Entry Box to new section
    Added new text entry box to the new section to display the API response and Send the Input to Fruityvice as Part of the API Call.

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,10 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json()) #just displays the data in raw json
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 
 
 # taking the json version and normalizing the response 
