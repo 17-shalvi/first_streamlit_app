7711280af89a7adc7565fac8d25ccf8ff1c1bc61
17-shalvi <71483138+17-shalvi@users.noreply.github.com>
2023-03-26 06:58:09 -0400

    fixed teh bug
    commented the line that displays raw json data

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ streamlit.dataframe(fruits_to_show)
 import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just displays the data in raw json
+#streamlit.text(fruityvice_response.json()) #just displays the data in raw json
 
 
 # taking the json version and normalizing the response 
