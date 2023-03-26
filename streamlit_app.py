5dc08a883bf7c42166f0bbcc93ac3153fcb1ea9e
17-shalvi <71483138+17-shalvi@users.noreply.github.com>
2023-03-26 06:54:21 -0400

    changing the API response from json
    + displaying the API response as table

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,9 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+
+# taking the json version and normalizing the response 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it in the screen as table 
+streamlit.dataframe(fruityvice_normalized)
